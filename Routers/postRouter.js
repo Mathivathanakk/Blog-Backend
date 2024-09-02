e923fdb714edf17a98ffd4afdb0621c42b519b8b
Mathivathanakk <mathikk06@gmail.com>
2024-09-02 15:22:38 +0530

    completed

--- Routers/postRouter.js
+++ Routers/postRouter.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {verifyToken} from '../Middlewares/verifyToken.js'
-import { createPost, getAllPost } from '../Controllers/postController.js';
+import { createPost, deletePost, getAllPost, updatePost } from '../Controllers/postController.js';
 
 
 const router=express.Router()
 
 router.post("/createpost",verifyToken,createPost)
 router.get('/getposts',getAllPost)
+router.delete('/deletepost/:postId/:userId',verifyToken,deletePost)
+router.put('/updatepost/:postId/:userId',verifyToken,updatePost)
 
 
 export default router;
